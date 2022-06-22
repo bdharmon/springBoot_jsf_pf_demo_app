88b732006d3e9d9dee30bbfa958e6e93a196d4b0
Brandon Harmon <34636740+bdharmon@users.noreply.github.com>
2022-06-22 12:02:19 -0400

    commit

---
+++ src/main/java/com/demo1/session/UserDetailsImpl.java
@@ -0,0 +1,58 @@
+package com.demo1.session;
+
+import com.demo1.entity.UserAccount;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final UserAccount userAccount;
+
+    public UserDetailsImpl(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return userAccount.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userAccount.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDetailsImpl{" +
+                "userAccount=" + userAccount +
+                '}';
+    }
+}
---
+++ src/main/java/com/demo1/session/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.demo1.session;
+
+import com.demo1.entity.UserAccount;
+import com.demo1.repository.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserAccount userAccount = userAccountRepository.findByUsername(username);
+
+        if (userAccount == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new UserDetailsImpl(userAccount);
+    }
+}
