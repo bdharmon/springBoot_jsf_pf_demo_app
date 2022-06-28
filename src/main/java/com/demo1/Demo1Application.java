a6bd19435c7a7ab40379ef9765e127c260e9ef30
Brandon Harmon <34636740+bdharmon@users.noreply.github.com>
2022-06-28 13:25:30 -0400

    commit

--- src/main/java/com/demo1/Demo1Application.java
+++ src/main/java/com/demo1/Demo1Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -20,15 +21,23 @@ public class Demo1Application {
 	@Bean
 	public CommandLineRunner demo(UserAccountRepository userAccountRepository) {
 		return (args) -> {
-			UserAccount user1 = new UserAccount("john", new BCryptPasswordEncoder().encode("test123"), "john@email.com");
-			UserAccount user2 = new UserAccount("bill", new BCryptPasswordEncoder().encode("test123"), "bill@email.com");
-			UserAccount user3 = new UserAccount("zack", new BCryptPasswordEncoder().encode("test123"), "zack@email.com");
-
-			userAccountRepository.saveAll(List.of(user1, user2, user3));
-
-			for (UserAccount userAccount : userAccountRepository.findAll()) {
-				System.out.println(userAccount);
+			List<UserAccount> myList = new ArrayList<>();
+			for (int i = 0; i <= 10000; i++) {
+				UserAccount user1 = new UserAccount(String.format("user_%s", i), new BCryptPasswordEncoder().encode("test123"), String.format("user_%s@email.com", i));
+				myList.add(user1);
 			}
+			System.out.println(myList);
+			userAccountRepository.saveAll(myList);
+
+//			UserAccount user1 = new UserAccount("john", new BCryptPasswordEncoder().encode("test123"), "john@email.com");
+//			UserAccount user2 = new UserAccount("bill", new BCryptPasswordEncoder().encode("test123"), "bill@email.com");
+//			UserAccount user3 = new UserAccount("zack", new BCryptPasswordEncoder().encode("test123"), "zack@email.com");
+
+//			userAccountRepository.saveAll(List.of(user1, user2, user3));
+
+//			for (UserAccount userAccount : userAccountRepository.findAll()) {
+//				System.out.println(userAccount);
+//			}
 		};
 	}
 
